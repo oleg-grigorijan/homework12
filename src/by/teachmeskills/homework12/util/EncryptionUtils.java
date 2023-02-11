58a86cba54d6ed096d17a7521c26b95f75732d93
Oleg Grigorijan <oleg.grigorijan@gmail.com>
2023-02-11 11:11:52 +0300

    Preparing declaration of Caesar functions

---
+++ src/by/teachmeskills/homework12/util/EncryptionUtils.java
@@ -0,0 +1,32 @@
+package by.teachmeskills.homework12.util;
+
+public class EncryptionUtils {
+
+    /**
+     * Encrypts given string with a Caesar cipher
+     *
+     * @param source String to be encrypted made of upper-case english letters only
+     * @param key    Secret value which means number of positions to shift the alphabet
+     * @return Encrypted string
+     * @throws IllegalArgumentException If source string contains unsupported characters
+     * @see <a href="https://en.wikipedia.org/wiki/Caesar_cipher">Caesar Chipher</a>
+     */
+    public static String encryptWithCaesar(String source, int key) {
+        // TODO: Implement
+        throw new IllegalStateException("Not yet implemented");
+    }
+
+    /**
+     * Decrypts given string with a Caesar cipher
+     *
+     * @param source String to be decrypted made of upper-case english letters only
+     * @param key    Secret value which means number of positions to shift the alphabet
+     * @return Decrypted string
+     * @throws IllegalArgumentException If source string contains unsupported characters
+     * @see <a href="https://en.wikipedia.org/wiki/Caesar_cipher">Caesar Chipher</a>
+     */
+    public static String decryptWithCaesar(String source, int key) {
+        // TODO: Implement
+        throw new IllegalStateException("Not yet implemented");
+    }
+}

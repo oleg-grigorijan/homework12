d89ae0d5165ea0ee25cfc7c86ba567044cc3a731
Anton Fedoseev <fedoseev446@gmail.com>
2023-02-14 15:23:13 +0300

    Initial commit

--- src/by/teachmeskills/homework12/util/EncryptionUtils.java
+++ src/by/teachmeskills/homework12/util/EncryptionUtils.java
@@ -1,15 +1,6 @@
 package by.teachmeskills.homework12.util;
 
 public class EncryptionUtils {
-    /**
-     * Encrypts given string with a Caesar cipher
-     *
-     * @param source String to be encrypted made of upper-case english letters only
-     * @param key    Secret value which means number of positions to shift the alphabet
-     * @return Encrypted string
-     * @throws IllegalArgumentException If source string contains unsupported characters
-     * @see <a href="https://en.wikipedia.org/wiki/Caesar_cipher">Caesar Chipher</a>
-     */
     public static final String ENGLISH_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static String encryptWithCaesar(String source, int key) {
         char[] sourceArr = source.toCharArray();
@@ -29,17 +20,6 @@ public class EncryptionUtils {
         }
         return resultCaesar;
     }
-
-
-    /**
-     * Decrypts given string with a Caesar cipher
-     *
-     * @param source String to be decrypted made of upper-case english letters only
-     * @param key    Secret value which means number of positions to shift the alphabet
-     * @return Decrypted string
-     * @throws IllegalArgumentException If source string contains unsupported characters
-     * @see <a href="https://en.wikipedia.org/wiki/Caesar_cipher">Caesar Chipher</a>
-     */
     public static String decryptWithCaesar(String source, int key) {
         return encryptWithCaesar(source, ENGLISH_ALPHABET.length() - (key % ENGLISH_ALPHABET.length()));
     }

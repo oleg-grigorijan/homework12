394862f2868d1c1cdd45a168a682dded284928f6
Anton Fedoseev <fedoseev446@gmail.com>
2023-02-17 12:18:31 +0300

    Final hw

--- src/by/teachmeskills/homework12/util/EncryptionUtils.java
+++ src/by/teachmeskills/homework12/util/EncryptionUtils.java
@@ -2,31 +2,30 @@ package by.teachmeskills.homework12.util;
 
 public class EncryptionUtils {
     public static final String ENGLISH_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     public static String encryptWithCaesar(String source, int key) {
+
         char[] sourceArr = source.toCharArray();
+
+        String resultCaesar = "";
 
         for (char ch : sourceArr) {
             if (!isLatinAndUpper(ch)) {
                 throw new IllegalArgumentException("If source string contains unsupported characters");
             }
-        }
-        String resultCaesar = "";
-        for (char ch : sourceArr) {
             if (ENGLISH_ALPHABET.indexOf(ch) != -1) {
-                ch = ENGLISH_ALPHABET.charAt((ENGLISH_ALPHABET.indexOf(ch) + key) % ENGLISH_ALPHABET.length());
+                ch = ENGLISH_ALPHABET.charAt(Math.floorMod(ENGLISH_ALPHABET.indexOf(ch) + key,
+                        ENGLISH_ALPHABET.length()));
                 resultCaesar += ch;
-
             }
         }
         return resultCaesar;
     }
     public static String decryptWithCaesar(String source, int key) {
-        return encryptWithCaesar(source, ENGLISH_ALPHABET.length() - (key % ENGLISH_ALPHABET.length()));
+        return encryptWithCaesar(source, -key);
     }
 
     public static boolean isLatinAndUpper(char inputChar) {
-        return Character.isUpperCase(inputChar) && ((inputChar >= 'A' && inputChar <= 'Z'));
+        return ((inputChar >= 'A' && inputChar <= 'Z'));
     }
-
-
 }

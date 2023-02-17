264f84db880fdd1f0de8663aebbe6d40203619fe
artempenkovskij <artempenkovsky@inbox.ru>
2023-02-17 19:20:58 +0300

    Merge remote-tracking branch 'origin/feature-caesar-artem-pe' into feature-caesar-artem-pe
    # Conflicts:
    #	src/by/teachmeskills/homework12/util/EncryptionUtils.java

--- src/by/teachmeskills/homework12/util/EncryptionUtils.java
+++ src/by/teachmeskills/homework12/util/EncryptionUtils.java
@@ -1,6 +1,7 @@
 package by.teachmeskills.homework12.util;
 
 public class EncryptionUtils {
+    static int sumALPHABET = 26;
     public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 
     /**
@@ -13,7 +14,6 @@ public class EncryptionUtils {
      * @see <a href="https://en.wikipedia.org/wiki/Caesar_cipher">Caesar Chipher</a>
      */
     public static String encryptWithCaesar(String source, int key) {
-        int sumALPHABET = 26;
         source = source.toLowerCase();
         String encryptStr = "";
 
@@ -42,25 +42,20 @@ public class EncryptionUtils {
     public static String decryptWithCaesar(String source, int key) {
         source = source.toLowerCase();
 
-        // decryptStr to store decrypted data
         String decryptStr = "";
 
-        // use for loop for traversing each character of the input string
         for (int i = 0; i < source.length(); i++)
         {
             int pos = ALPHABET.indexOf(source.charAt(i));
-            int decryptPos = (pos - key) % 26;
+            int decryptPos = (pos - key) % sumALPHABET;
 
-            // if decryptPos is negative
             if (decryptPos < 0){
                 decryptPos = ALPHABET.length() + decryptPos;
             }
             char decryptChar = ALPHABET.charAt(decryptPos);
 
-            // add decrypted char to decrypted string
             decryptStr += decryptChar;
         }
-        // return decrypted string
         return decryptStr;
     }
 }
